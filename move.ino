28b3c483ab9c94825140193bc7431b427daf1a77
mdmckinnon <47580427+mdmckinnon@users.noreply.github.com>
2020-10-10 17:35:27 -0600

    Update move.ino
    Fixed error in calculating angles, now is just 180/ pi not 180 / pi halves. Removed millis() timing and replaced with simple delay().

--- move.ino
+++ move.ino
@@ -3,15 +3,15 @@
 
 // initalize vars
 int angles[] = {90, 90, 90, 90}; // Edit these if you want to change the starting position of the arm
-
 float arm1 = 100.0;
 float arm2 = 100.0;
-float endEffector = 0; // Update with the length from the grapsing point to the pivot
-int servoSpeed = 50; // Change this to incease or decrease speed of movement
+float endEffector = 0; // Change to the length from the grapsing point to the pivot
+int servoSpeed = 2; // Change this to incease or decrease speed of movement
 
 
 //Servos to be used
 Servo servos[4];
+int servopos[] = {90, 90, 90, 90};
 // servos[0] Base rotation
 // servos[1] Main arm pivot
 // servos[2] Middle joint 
@@ -23,51 +23,49 @@ void setup() {
 	servos[1].attach(9);
 	servos[2].attach(10);
 	servos[3].attach(11);
-
+  
 	moveArm(); // Initializes arm into starting position
 }
 
 void loop() {
-	// put your main code here, to run repeatedly:
-	//getCoordinates();
-	calculateMove(-500, 200, -50);
+	calculateMove(-150, 75, -50);
 	moveArm();
+	delay(2000);
 	
-	delay(1000);
-	
-	calculateMove(150, 100, 0);
+	calculateMove(75, 50, 50);
 	moveArm();
-	delay(1000);
+	delay(2000);
+
+	calculateMove(100, 150, -50);
+	moveArm();
+	delay(2000);
 }
 
-// Gets the position data for the x,y,z points
-// Y >= 0; 
+// Gets the position data for the x,y,z points 
 //void getXYZ() {
 //}
 
-// Uses updated values for theta 1 through 4 and moves servos accordingly
-// Borref gave me the inspiration for this approach
+// Uses stored values for angles 1 through 4 and moves servos to position
 void moveArm() {
-	bool completed[] = {false, false, false, false}; // bool to check if all servos are in position
-	long prevTime = millis();
+	bool completed[] = {false, false, false, true}; // bool to check if all servos are in position
 	int pos;
   
-	//Loops until all the servos have reached thier desired position
-	while (!completed[0] && !completed[1] && !completed[2]){
-		if (millis() - prevTime > servoSpeed) {
-			prevTime = millis();
-			for (byte i = 0; i < 4; i++) {
-				pos = servos[i].read();
-				if (angles[i] > pos) {
-					servos[i].write(pos + 1);
-				}
-				else if (angles[i] < pos) {
-					servos[i].write(pos - 1);
-				}
-				else {
-					completed[i] = true;
-				}
+	// Loops until all the servos have reached thier desired position
+	while (!completed[0] || !completed[1] || !completed[2] || !completed[3]){
+      
+		// Loops through each servos, changes position 1 degree at a time
+		for (byte i = 0; i < 4; i++) {
+			pos = servos[i].read();
+			if (angles[i] > pos && completed[i] == false) {
+				servos[i].write(pos + 1);
 			}
+			else if (angles[i] < pos && completed[i] == false) {
+				servos[i].write(pos - 1);
+			}
+			else {
+				completed[i] = true;
+			}
+		delay(servoSpeed);
 		}
 	}
 }
@@ -79,32 +77,35 @@ void calculateMove(int x, int y, int z) {
 	float theta2 = 0;
 	float theta3 = 0;
 	float theta4 = 0;
-	float vLength = sqrt(x * x + y * y + z * z);
+	float vLength = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
 
 	// Calc theta 1
-	if (x >= 0) {
-		theta1 = 90.0 - (atan(x/y) * 180.0 / M_PI_2);
+	if (x > 0) {
+		theta1 = (atan2(y, x) * 180.0 / M_PI);
 	}
 	else {
-		theta1 = 90.0 + (atan(abs(x)/y) * 180.0 / M_PI_2);
+		theta1 = (atan2(-x, y) * 180.0 / M_PI) + 90;
 	}
 
 	// Calc theta 2
-	float phi1 = acos((arm1 * arm1 + vLength * vLength - arm2 * arm2)/ (2 * arm1 * vLength));
-	float phi2 = atan(z / sqrt(x * x + y * y));
-	theta2 = (phi1  + phi2) * 180.0 / M_PI_2;
+	theta2 = acos((pow(arm1, 2) + pow(vLength, 2) - pow(arm2, 2))/ (2 * arm1 * vLength));
+	theta2 = (theta2 + asin(z / vLength)) * 180.0 / M_PI;
 
 	// Calc theta 3
-	phi1 = acos((arm1 * arm1 + arm2 * arm2 - vLength * vLength) / (2 * arm1 * arm2));
-	theta3 = phi1 * 180.0 / M_PI_2;
-
-	//Updating angles of the servos
+	theta3 = acos((pow(arm1, 2) + pow(arm2, 2) - pow(vLength, 2)) / (2 * arm1 * arm2));
+	theta3 = theta3 * 180.0 / M_PI;
+	
+	// Calc theta 4
+	//theta4 = 0;
+	
+	// Updating angles of the servos
 	angles[0] = round(180 - theta1);
-	angles[1] = round(theta2 * 2);
+	angles[1] = round(180 - (theta2 * 2));
 	angles[2] = round((theta3 - 52.5) * 180 / 127.5);
 	angles[3] = 0;
-
- //Checks to ensure robot doesn't extend past limits, other checking is implemented in get xyz
+  
+ // Checks to ensure robot doesn't extend past limits
+ // Need to update to better handle errors, send message back
  for (byte i = 0; i < 4; i++) {
 	if (angles[i] >= 180) {
 		angles[i] = 179;
